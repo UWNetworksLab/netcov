f8ba86ae6700315d8e3be402554875c468b7a0ac
Xieyang Xu <ashlippers@gmail.com>
2022-08-31 01:04:12 -0700

    pypi release 0.1.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup, find_packages
 setup(
     name='netcov',
     version='0.1.0',
-    description='Prototype of control plane coverage analyzer',
+    description='Coverage analyzer for network router configurations',
     author='Xieyang Xu',
     author_email='ashlippers@gmail.com',
     packages=find_packages(exclude=('tests')),
